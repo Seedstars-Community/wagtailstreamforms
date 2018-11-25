535f875808d8190bf1a38eea6f8c5d960fa6164e
amir <amir.anwar.said@gmail.com>
2018-11-25 10:31:07 +0200

    Fix: field name change breaks previous form submissions + updated for compatability with react streamfields

--- wagtailstreamforms/models/submission.py
+++ wagtailstreamforms/models/submission.py
@@ -1,8 +1,10 @@
 import json
+from copy import deepcopy
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from wagtailstreamforms.utils.general import get_slug_from_string
 
 class FormSubmission(models.Model):
     """ Data for a form submission. """
@@ -24,10 +26,24 @@ class FormSubmission(models.Model):
         """ Returns dict with form data. """
         form_data = json.loads(self.form_data)
 
+        # getting a copy to iterate over, without touching the original dict
+        data_copy = deepcopy(form_data)
+        
+        form_fields = self.form.get_form_fields()
+        
+        # Swapping uuid fields with field labels
+        for key in data_copy:
+            for field in form_fields:
+                if  field['id'] == key:
+                    new_key = get_slug_from_string(field['value']['label'])
+                    # swapping keys
+                    form_data[new_key] = form_data.pop(key)
+                    break
+
         form_data.update(
             {'submit_time': self.submit_time, }
         )
-
+        
         return form_data
 
     def __str__(self):
